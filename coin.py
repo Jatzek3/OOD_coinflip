175df33f0f28aecffa8da3a2ec660418002f3576
Jacek Kawalec <jokerchocolate@gmail.com>
2020-01-31 23:48:28 +0100

    Coin an Players class works correctly

--- coin.py
+++ coin.py
@@ -3,15 +3,15 @@ import random
 
 class Coin:
 
-    def __init__(self, coin_option):
-        self.coin_option = coin_option
-        self.coin_values = ("Heads", "Tails")
+    def __init__(self, coin_option=None):       # We must initialize an instance of coin
+        self.coin_option = coin_option          # It will always have to have te side value(default None)
+        self.coin_values = ("Heads", "Tails")   # The coin have 2 sides Heads and Tails
 
-    def get_coin_option(self):
-        self.coin_option = random.choice(self.coin_values)
+    def get_coin_option(self):                  # Method which chooses which side of coin will be on top
+        self.coin_option = random.choice(self.coin_values)  # We flip it from Coin.coin values sides
+        return self.coin_option                 # And return it so coin will now have random value
+
+    def __str__(self):                          # Printing the instance for debug
         return self.coin_option
-
-    def __str__(self):
-        return print(self.coin_option)
 
 
--- player.py
+++ player.py
@@ -1,27 +1,43 @@
 import random
-
 import coin_game
 import coin
 
+
 class Player:
 
-    def __init__(self, name, coin_option):
-        self. name = name
-        self.coin_option = coin_option
+    def __init__(self, name, coin_option=None):             # We set up a player name and default value to None
+        self. name = name                                   # Name will have to be inserted when the instance is created
+        self.coin_option = coin_option                      # But coin option not since it defaults to None
+        self.coin_values = ("Heads", "Tails")
 
+    def set_coin_option(self):                              # player chooses an option
+        self.coin_option = random.choice(self.coin_values)  # both values can be chosen by player
+        return self.coin_option                             # We are returning chosen value
 
-    def set_coin_option(self):  # player chooses an option
-        return self.CoinOption
+    def get_coin_option(self, other):                       # Other player chooses the player will get second choice
+        if other.coin_option == "Heads":                    # If other player chose Heads ->
+            self.coin_option = "Tails"                      # This player will get Tails
+        else:
+            self.coin_option = "Heads"                      # Otherwise it will get Heads
+        return self.coin_option                             # The coin value is now appropriate
 
-    def get_coin_option(self):  # player get the other option
-        return self.CoinOption
-
-    def get_random_coin_option(self): # option when player decides on the side
-        return self.CoinOption
-
-
-    def did_playet_Win(self):
+    def did_player_win(self, coin):
+        if self.coin_option == coin.coin_option:
+            print(f"{self.name} has won the coin toss")
         return None
 
-    def __str__(self):
-        return print(self.name),print(self.coin_option)
+    def __str__(self):                                      # print for debug
+        return self.name +" "+ self.coin_option
+
+
+player1 = Player("Mark")
+player2 = Player("Tom")
+player1.set_coin_option()
+player2.get_coin_option(player1)
+print(player1)
+print(player2)
+coin = coin.Coin()
+coin.get_coin_option()
+print(coin)
+player1.did_player_win(coin)
+player2.did_player_win(coin)
